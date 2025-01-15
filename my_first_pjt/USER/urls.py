10649d0cfde4ca6f0bc241bf4aa5e8448ae18911
“personal” <ouxrlo3@gmail.com>
2025-01-15 20:25:25 +0900

    def

--- my_first_pjt/USER/urls.py
+++ my_first_pjt/USER/urls.py
@@ -5,9 +5,7 @@ from . import views
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('users/', views.users, name='users'),
-    path('login/', views.login, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile, name='profile'),
-    path('login/', auth_views.LoginView.as_view(template_name='USER/login.html'), name='login'),
     path('home/', views.index, name='home'),
 ]
