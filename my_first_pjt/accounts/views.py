18075b0f632e5d46f753096eff061d1b4c2d4a39
“personal” <ouxrlo3@gmail.com>
2025-01-15 17:17:02 +0900

    update_login_view

--- my_first_pjt/accounts/views.py
+++ my_first_pjt/accounts/views.py
@@ -5,26 +5,22 @@ from django.contrib.auth.forms import AuthenticationForm
 from .forms import LoginForm, SignupForm
 
 
-# 로그인 뷰
+    
+
 def login_view(request):
-    if request.method == 'POST':
-        form = LoginForm(request, data=request.POST)  # LoginForm을 사용하여 POST 데이터 받기
+    if request.method == "POST":
+        form = LoginForm(request.POST)
         if form.is_valid():
-            # form.cleaned_data에서 유효한 데이터를 받아옴
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            
-            # 사용자 인증
-            user = authenticate(username=username, password=password)
-            
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
             if user is not None:
-                # 사용자가 인증되면 로그인 처리
                 login(request, user)
-                return redirect('index')  # 로그인 후 리디렉션할 URL, 'index'로 설정
+                return redirect('user_profile', username=user.username)
             else:
-                form.add_error(None, '아이디 또는 비밀번호가 맞지 않습니다.')
+                form.add_error(None, '아이디 또는 비밀번호가 잘못되었습니다.')
     else:
-        form = LoginForm()  # GET 요청 시 폼을 초기화
+        form = LoginForm()
 
     return render(request, 'accounts/login.html', {'form': form})
 
